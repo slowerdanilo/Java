054be540e2096b2917753d29e643d92340eab5ee
Danilo Silva de Melo <80988811+slowerdanilo@users.noreply.github.com>
2023-02-10 17:52:26 -0300

    Add files via upload

---
+++ EstruturaFor/src/main/java/EstruturaFor/Main.java
@@ -0,0 +1,13 @@
+package EstruturaFor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <=10; i++){
+            
+        }
+        
+    }
+    
+}
---
+++ EstruturaWhile/src/main/java/EstruturaWhile/Main.java
@@ -0,0 +1,27 @@
+package EstruturaWhile;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int totalAlunos = 10;
+        
+        Scanner leitorScanner = new Scanner(System.in);
+        
+        while(totalAlunos > 0){
+        
+            String nomeAluno = leitorScanner.nextLine();
+            int idadeAluno = leitorScanner.nextInt();
+            
+            System.out.println("O nome do aluno é: " + nomeAluno + " e sua "
+                    + "idade é " + idadeAluno);
+            
+            totalAlunos = totalAlunos - 1;
+            
+        }                
+        
+    }
+    
+}
